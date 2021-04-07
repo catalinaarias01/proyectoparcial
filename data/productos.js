ab2d8797e952ec828448111172f9fd58eb0a9344
catalinaarias01 <63122855+catalinaarias01@users.noreply.github.com>
2021-04-06 23:11:31 -0300

    Cargue productos en productos.js

--- data/productos.js
+++ data/productos.js
@@ -3,33 +3,111 @@
 const productos = [
     {
         id:0,
-        nombre:"labial",
-        descripción: "producto para los labios",
-        img:"/images/products/labial.jpeg"
+        nombre:"Mini Badger Brush Set",
+        descripcion: "• Pincel Spoolie • Shadow Brush  • Eyebrush  • Angle Liner  • Lip Brush - • Blush Brush • Powder Brush • Portacosmeticos",
+        seccion: "Brochas",
+        marca: "Morphe",
+        precio: "$3400",
+        img:"/images/products/brochas-morphe.jpg"
     },
     {
         id:1,
-        nombre:"rimel",
-        descripción: "producto para los ojos",
-        img:"/images/products/rimel.jpeg"
+        nombre:"Cest La Vie Palette",
+        descripcion: "Paleta de 30 sombras - Cruelty Free",
+        seccion: "Ojos",
+        marca: "Rude Cosmetics",
+        precio: "$4600",
+        img:"/images/products/cestlavie-paleta.jpg"
     },
     {
         id:2,
-        nombre:"delineador",
-        descripción: "producto para los ojos",
-        img:"/images/products/delineador.jpeg"
+        nombre:"Flawless Finish Foundation",
+        descripcion: "La Flawless foundation es cremosa, ligera y libre de aceites. Se difumina facilmente con una brocha o esponjita y da un acabado semi-matte que dura todo el día. Sirve para todo tipo de piel!",
+        seccion: "Rostro",
+        marca: "e.l.f",
+        precio: "$2900",
+        img:"/images/products/flawlessFinishFoundation.jpg"
     },
     {
         id:3,
-        nombre:"rubor",
-        descripción: "producto para los cachetes",
-        img: "/images/products/rubor.jpeg"
+        nombre:"Ultra Matte Lipstick",
+        descripcion: "Labial formato líquido, de larga duración y cruelty free",
+        seccion: "Labios",
+        marca: "ColourPop",
+        precio: "$1000",
+        img: "/images/products/ultraMatteLip.jpg"
     },
     {
         id:4,
-        nombre:"crema",
-        descripción: "producto para las manos",
-        img:"/images/products/labial.jpeg"
+        nombre:"Lustre Lipstick",
+        descripcion: "El icónico producto que hizo famoso a M·A·C. Esta fórmula liviana se desliza fácilmente, tiene una cobertura ligera-media y un acabado con semi-brillo.",
+        seccion: "Labios",
+        marca: "MAC",
+        precio: "$2100",
+        img:"/images/products/lustreLipstick.jpg"
+    },
+    {
+        id:5,
+        nombre: "Poreless Face Primer",
+        descripcion: "Blemish control: Ayuda a controlar los brotes con el uso. Tiene una textura suave y deja un acabado matte en la piel.",
+        seccion: "Rostro",
+        marca: "e.l.f",
+        precio: "$2250",
+        img: "/images/products/porelessFacePrimer.jpg"
+    },
+    {
+        id:6,
+        nombre: "Panoramic Mascara",
+        descripcion: "Esta máscara de pestañas, que agrega volumen instantáneo a tus pestañas, combina un pigmento intensamente negro con ingredientes nutritivos para brindar pestañas espectaculares y saludables en una sola pasada.",
+        seccion: "Ojos",
+        marca: "Laura Mercier",
+        precio: "$1800",
+        img: "/images/products/panoramicMascara.jpg"
+    },
+    {
+        id:7,
+        nombre: "Powder Brush",
+        descripcion: "Brocha de pelo natural ideal para aplicar rubor o para sellar zonas altas con polvo",
+        seccion: "Brochas",
+        marca: "Coastal Scents",
+        precio: "$950",
+        img: "/images/products/powderBrush.jpg"
+    },
+    {
+       id:8,
+       nombre: "Face and Body",
+       descripcion: "Una base líquida liviana y confortable con una combinación única de emolientes con cobertura media en capas y un acabado satinado natural. La fórmula humectante de larga duración se desliza sobre el rostro y el cuerpo con un color que no se desvanece.",
+       seccion: "Rostro",
+       marca: "MAC",
+       precio: "$4350",
+       img: "/images/products/faceAndBody.jpg"
+    },
+    {
+        id:9,
+        nombre: "Hoola Matte Bronzer",
+        descripcion: "Hoola Matte Bronzer le dará a tu rostro una luminosidad de beso de sol, es ideal para contornear, incluye una suave brocha de cerdas naturales.",
+        seccion: "Rostro",
+        marca: "Benefit",
+        precio: "$7250",
+        img: "/images/products/hoolaMatteBronzer.jpg"
+    },
+    {
+        id:10,
+        nombre: "Mattemoiselle Plush Lipstick",
+        descripcion: "Un lápiz labial ultra-delgado con un acabado mate suave como un pétalo y de larga duración, creado en una gama de 14 tonos desde ligeros, hasta intensos, cada uno diseñado para favorecer todos los tonos de la piel.",
+        seccion: "Labios",
+        marca: "Fenty Beauty",
+        precio: "$3800",
+        img: "/images/products/mattemoisellePlush.jpg"
+    },
+    {
+        id:11,
+        nombre:"James Charles Palette",
+        descripcion:"Paleta de 39 sombras Mixtas, Tamaño 28 x 20 Cm",
+        seccion: "Ojos",
+        marca: "Morphe",
+        precio: "$12300",
+        img: "/images/products/jamesCharlesPalette.jpg"
     }
 ]
 
