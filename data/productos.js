d4d85bfcd7e3fc8247a85ff98fdbeb6ae6f2c888
Maria Peña <mpena@udesa.edu.ar>
2021-04-15 12:45:24 -0300

    tabla comentarios y usuarios

--- data/productos.js
+++ data/productos.js
@@ -1,4 +1,7 @@
 //Acá debería haber un array con varios objetos (producto1: nombre, descripción, link a imagen)
+const comentarios = require('./comentarios');
+
+
 const productos = [
     {
         id:0,
@@ -6,15 +9,28 @@ const productos = [
         descripcion: "• Pincel Spoolie • Shadow Brush  • Eyebrush  • Angle Liner  • Lip Brush - • Blush Brush • Powder Brush • Portacosmeticos",
         seccion: "Brochas",
         marca: "Morphe",
-        img:"/images/products/brochas-morphe.jpg"
+        img:"/images/products/brochas-morphe.jpg",
+        comentarios : [
+            {
+                texto:comentarios[0],
+                usuario: comentarios[0],
+            },
+            {
+                texto:comentarios[1],
+                usuario: comentarios[2]
+            },
+        ]
     },
+        
+        
     {
         id:1,
         nombre:"Cest La Vie Palette",
         descripcion: "Paleta de 30 sombras - Cruelty Free",
         seccion: "Ojos",
         marca: "Rude Cosmetics",
-        img:"/images/products/cestlavie-paleta.jpg"
+        img:"/images/products/cestlavie-paleta.jpg",
+        comentarios : [comentarios[3], comentarios[4], comentarios[5]]
     },
     {
         id:2,
@@ -22,7 +38,8 @@ const productos = [
         descripcion: "Cremosa, ligera y libre de aceites. Da un acabado semi-matte que dura todo el día. Sirve para todo tipo de piel!",
         seccion: "Rostro",
         marca: "e.l.f",
-        img:"/images/products/flawlessFinishFoundation.jpg"
+        img:"/images/products/flawlessFinishFoundation.jpg",
+        comentarios : [comentarios[6], comentarios[7], comentarios[8]]
     },
     {
         id:3,
@@ -30,7 +47,8 @@ const productos = [
         descripcion: "Labial formato líquido, de larga duración y cruelty free",
         seccion: "Labios",
         marca: "ColourPop",
-        img: "/images/products/ultraMatteLip.jpg"
+        img: "/images/products/ultraMatteLip.jpg",
+        comentarios : [comentarios[9], comentarios[10], comentarios[0]]
     },
     {
         id:4,
@@ -38,7 +56,8 @@ const productos = [
         descripcion: "El icónico producto que hizo famoso a M·A·C. Esta fórmula liviana se desliza fácilmente, tiene una cobertura ligera-media y un acabado con semi-brillo.",
         seccion: "Labios",
         marca: "MAC",
-        img:"/images/products/lustreLipstick.jpg"
+        img:"/images/products/lustreLipstick.jpg",
+        comentarios : [comentarios[1], comentarios[2], comentarios[3]]
     },
     {
         id:5,
@@ -46,7 +65,8 @@ const productos = [
         descripcion: "Blemish control: Ayuda a controlar los brotes con el uso. Tiene una textura suave y deja un acabado matte en la piel.",
         seccion: "Rostro",
         marca: "e.l.f",
-        img: "/images/products/porelessFacePrimer.jpg"
+        img: "/images/products/porelessFacePrimer.jpg",
+        comentarios :[comentarios[4], comentarios[5], comentarios[6]]
     },
     {
         id:6,
@@ -54,7 +74,8 @@ const productos = [
         descripcion: "Esta máscara de pestañas, que agrega volumen instantáneo a tus pestañas, combina un pigmento intensamente negro con ingredientes nutritivos para brindar pestañas espectaculares y saludables en una sola pasada.",
         seccion: "Ojos",
         marca: "Laura Mercier",
-        img: "/images/products/panoramicMascara.jpg"
+        img: "/images/products/panoramicMascara.jpg",
+        comentarios : [comentarios[7], comentarios[8], comentarios[10]]
     },
     {
         id:7,
@@ -62,7 +83,8 @@ const productos = [
         descripcion: "Brocha de pelo natural ideal para aplicar rubor o para sellar zonas altas con polvo",
         seccion: "Brochas",
         marca: "Coastal Scents",
-        img: "/images/products/powderBrush.jpg"
+        img: "/images/products/powderBrush.jpg",
+        comentarios : [comentarios[1], comentarios[2], comentarios[3]]
     },
     {
        id:8,
@@ -70,7 +92,8 @@ const productos = [
        descripcion: "Una base líquida liviana y confortable con una combinación única de emolientes con cobertura media en capas y un acabado satinado natural. La fórmula humectante de larga duración se desliza sobre el rostro y el cuerpo con un color que no se desvanece.",
        seccion: "Rostro",
        marca: "MAC",
-       img: "/images/products/faceAndBody.jpg"
+       img: "/images/products/faceAndBody.jpg",
+       comentarios : [comentarios[4], comentarios[5], comentarios[6]]
     },
     {
         id:9,
@@ -78,7 +101,8 @@ const productos = [
         descripcion: "Hoola Matte Bronzer le dará a tu rostro una luminosidad de beso de sol, es ideal para contornear, incluye una suave brocha de cerdas naturales.",
         seccion: "Rostro",
         marca: "Benefit",
-        img: "/images/products/hoolaMatteBronzer.jpg"
+        img: "/images/products/hoolaMatteBronzer.jpg",
+        comentarios :[comentarios[7], comentarios[8], comentarios[9]]
     },
     {
         id:10,
@@ -86,7 +110,8 @@ const productos = [
         descripcion: "Un lápiz labial ultra-delgado con un acabado mate suave como un pétalo y de larga duración, creado en una gama de 14 tonos desde ligeros, hasta intensos, cada uno diseñado para favorecer todos los tonos de la piel.",
         seccion: "Labios",
         marca: "Fenty Beauty",
-        img: "/images/products/mattemoisellePlush.jpg"
+        img: "/images/products/mattemoisellePlush.jpg",
+        comentarios : [comentarios[10], comentarios[2], comentarios[3]]
     },
     {
         id:11,
@@ -94,7 +119,8 @@ const productos = [
         descripcion:"Paleta de 39 sombras Mixtas, Tamaño 28 x 20 Cm",
         seccion: "Ojos",
         marca: "Morphe",
-        img: "/images/products/jamesCharlesPalette.jpg"
+        img: "/images/products/jamesCharlesPalette.jpg",
+        comentarios : [comentarios[4], comentarios[5], comentarios[7]]
     }
 ]
 
---
+++ data/usuarios.js
@@ -0,0 +1,54 @@
+const usuarios = [
+    {
+        id:0,
+        nombre:"Cata",
+        apellido:"Quian",
+        usuario:"cataquian",
+        mail:"cataquian@gmail.com",
+        edad:90,
+        contraseña:"gato",
+        fecha_creación: new Date()
+    },
+    {
+        id:1,
+        nombre:"Cata",
+        apellido:"Quian",
+        usuario:"mariapena",
+        mail:"cataquian@gmail.com",
+        edad:90,
+        contraseña:"gato",
+        fecha_creación: new Date()
+    },
+    {
+        id:2,
+        nombre:"Cata",
+        apellido:"Quian",
+        usuario:"cataarias",
+        mail:"cataquian@gmail.com",
+        edad:90,
+        contraseña:"gato",
+        fecha_creación: new Date()
+    },
+    {
+        id:3,
+        nombre:"Cata",
+        apellido:"Quian",
+        usuario:"felirattagan",
+        mail:"cataquian@gmail.com",
+        edad:90,
+        contraseña:"gato",
+        fecha_creación: new Date()
+    },
+    {
+        id:4,
+        nombre:"Cata",
+        apellido:"Quian",
+        usuario:"fedeprieto",
+        mail:"cataquian@gmail.com",
+        edad:90,
+        contraseña:"gato",
+        fecha_creación: new Date()
+    },
+]
+
+module.exports = usuarios;
