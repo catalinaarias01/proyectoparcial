984a28143042759c1397d90ae7acc740f35acebe
catalinaarias01 <63122855+catalinaarias01@users.noreply.github.com>
2021-04-26 12:55:14 -0300

    Borre rutas y controladores que no sirven

--- routes/profile.js
+++
@@ -1,8 +0,0 @@
-/* var express = require('express');
-var router = express.Router();
-const profileController = require('../controllers/profileController')
-
-router.get('/', profileController.index);
-router.get('/edit', profileController.edit);
-
-module.exports = router; */
