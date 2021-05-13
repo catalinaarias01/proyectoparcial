82d4ebd8220ff3c49b92842ca7bd6f1d4e80e9c2
catalinaarias01 <63122855+catalinaarias01@users.noreply.github.com>
2021-05-13 12:05:02 -0300

    Usuarios.js

--- database/models/Usuarios.js
+++ database/models/Usuarios.js
@@ -1,6 +1,39 @@
 module.exports = (sequelize, dataTypes)=>{
     let alias = "Usuarios";
-    let cols = {};
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER,
+            allowNull: false,
+        },
+        nombre: {
+            type: dataTypes.STRING,
+            allowNull: false,
+        },
+        apellido: {
+            type: dataTypes.STRING,
+            allowNull: false,
+        },
+        mail: {
+            type: dataTypes.STRING,
+            allowNull: false,
+        },
+        nombre_usuario: {
+            type: dataTypes.STRING,
+            allowNull: false,
+        },
+        edad: {
+            type: dataTypes.TINYINT,
+        },
+        contraseña: {
+            type: dataTypes.STRING,
+        },
+        fecha_creacion:{
+            type: dataTypes.DATE,
+            allowNull: false,
+        },
+    };
     let config = {
         table: "usuarios",
         timestamps: false,
