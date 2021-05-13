f685706582c2001b81ae27189c59d431a90b9035
mariapenaa <mpena@udesa.edu.ar>
2021-05-13 12:03:14 -0300

    modelo

--- database/models/Cliente_producto.js
+++ database/models/Cliente_producto.js
@@ -1,6 +1,22 @@
 module.exports = (sequelize, dataTypes)=>{
     let alias = "Cliente_producto";
-    let cols = {};
+    let cols = {
+        id:{
+            primaryKey:true,
+            autoIncrement:true,
+            type:dataTypes.INTEGER,
+            allowNull:false
+        },
+        producto_id:{
+            type:dataTypes.INTEGER,
+            allowNull:false
+
+        },
+        usuario_id:{
+            type:dataTypes.INTEGER,
+            allowNull:false
+        }
+    };
     let config = {
         table: "cliente_producto",
         timestamps: false,
--- database/models/Comentarios.js
+++ database/models/Comentarios.js
@@ -1,9 +1,33 @@
 module.exports = (sequelize, dataTypes)=>{
     let alias = "Comentarios";
-    let cols = {};
+    let cols = {
+        id:{
+            primaryKey:true,
+            autoIncrement:true,
+            type:dataTypes.INTEGER,
+            allowNull:false,
+ 
+        },
+        texto:{
+            type:dataTypes.STRING,
+            allowNull:false,
+        },
+        fecha_creacion:{
+            type:dataTypes.DATE,
+            allowNull:false,
+        },
+        producto_id:{
+            type:dataTypes.INT,
+            allowNull:false,
+        },
+        usuario_id:{
+            type:dataTypes.INT,
+            allowNull:false,
+        }
+    };
     let config = {
         table: "comentarios",
-        timestamps: false,
+        timestamps: true,
         underscored: true
     }
 
