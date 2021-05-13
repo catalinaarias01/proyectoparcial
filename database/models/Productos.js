cd0963a9908f25f8ce02a751fdcb1a351706c1cd
Felicitas Rattagan <frattagan@udesa.edu.ar>
2021-05-13 12:04:38 -0300

    productos database

--- database/models/Productos.js
+++ database/models/Productos.js
@@ -1,9 +1,48 @@
 module.exports = (sequelize, dataTypes)=>{
     let alias = "Productos";
-    let cols = {};
+    let cols = {
+        id:{
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER,
+            allowNull:false
+        },
+        nombre_producto:{
+            type:dataTypes.STRING,
+            allowNull:false
+        },
+        descripcion:{
+            type:dataTypes.STRING,
+            allowNull:false
+        },
+        seccion:{
+            type:dataTypes.STRING,
+            allowNull:false
+        },
+        marca:{
+            type:dataTypes.STRING,
+            allowNull:false
+        },
+        img_url:{
+            type:dataTypes.STRING,
+            allowNull:false
+        },
+        fecha_creacion:{
+            type: dataTypes.DATE,
+            allowNull:false
+        },
+        usuario_id:{
+            type: dataTypes.INTEGER,
+            allowNull:false
+        },
+        comentarios_id:{
+            type: dataTypes.INTEGER,
+            allowNull:true
+        }
+    };
     let config = {
         table: "productos",
-        timestamps: false,
+        timestamps: true,
         underscored: true
     }
 
