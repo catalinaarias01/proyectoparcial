a050cc2729ba5fd5a5ca06b6648b1a0b5e8b0b06
catalinaarias01 <63122855+catalinaarias01@users.noreply.github.com>
2021-06-05 13:09:14 -0300

    Validaciones login y register

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -16,47 +16,129 @@ let usersController = {
 
     register: {
         index: (req, res) =>{
-            res.render('register')
+            // Control de acceso
+            if(req.session.user != undefined){
+                return res.redirect('/')
+            } else {
+                return res.render('register')
+            }
         },
         store: (req, res) =>{
-             let usuario = {
-                nombre: req.body.nombre,
-                apellido: req.body.apellido,
-                mail: req.body.email,
-                nombre_usuario:req.body.usuario,
-                edad:req.body.edad,
-                contraseña: bcrypt.hashSync(req.body.contraseña, 10),
-                fecha_creación:"2020-06-06 10:55:00",
-                img_usuario:""
-            } 
-            console.log(usuario)
-          usuarios.create(usuario)
-            .then(user => {
-                res.redirect('/users/login')
-                console.log(user)
-            })
-            .catch( error => console.log(error)) 
-        },
+
+            let errors = {};
+            //chequear los campos obligatorios
+            
+            if(req.body.nombre == ""){ // El nombre no debe esta vacio
+                errors.register = "Nombre no puede estar vacio"
+                res.locals.errors = errors
+    
+                return res.render('register')
+            }else if(req.body.apellido == ""){ // El apellido no debe esta vacio
+                errors.register = "Apellido no puede estar vacio"
+                res.locals.errors = errors
+        
+                return res.render('register')    
+            }else if(req.body.email == ""){ // El mail no debe esta vacio
+                errors.register = "Email no puede estar vacio"
+                res.locals.errors = errors
+    
+                return res.render('register')
+            }else if(req.body.usuario == ""){ // El usuario no debe esta vacio
+                errors.register = "Usuario no puede estar vacio"
+                res.locals.errors = errors
+        
+                return res.render('register')
+            }else if(req.body.edad == ""){ // La edad no debe esta vacia
+                errors.register = "Edad no puede estar vacio"
+                res.locals.errors = errors
+        
+                return res.render('register')
+            } else if (req.body.contraseña == ""){ // El password no este vacio
+                errors.register = "Contraseña no puede estar vacio"
+                res.locals.errors = errors
+    
+                return res.render('register')
+            } else if(req.body.repetir == ""){
+                errors.register = "Re escribir contraseña no puede estar vacio"
+                res.locals.errors = errors
+    
+                return res.render('register')
+            } else {
+    
+               usuarios.findOne({where: [{ mail : req.body.email}]})
+                .then( usuario => {
+                    if(usuario !=null){
+                        errors.register = "Email ya existe"
+                        res.locals.errors = errors
+    
+                        return res.render('register')
+                    } else if(req.body.contraseña != req.body.repetir ) {
+                        errors.register = "Las contraseñas no coinciden"
+                        res.locals.errors = errors
+    
+                        return res.render('register')
+                    } else {
+                        let usuario = {
+                            nombre: req.body.nombre,
+                            apellido: req.body.apellido,
+                            mail: req.body.email,
+                            nombre_usuario:req.body.usuario,
+                            edad:req.body.edad,
+                            contraseña: bcrypt.hashSync(req.body.contraseña, 10),
+                            fecha_creación:"2020-06-06 10:55:00",
+                            img_usuario:""
+                        }
+                        console.log(usuario)
+                        usuarios.create(usuario)
+                            .then( user => {
+                                return res.redirect('/users/login')
+                                console.log(user)
+                            })
+                            .catch( err => console.log(err))
+                    }
+                })
+                .catch( err => console.log(err)) 
+            }
+        }
     },
 
     login: {
         index: (req, res) =>{
-            res.render('login')
+            // Control de acceso
+            if(req.session.user != undefined){
+                return res.redirect('/')
+            } else {
+                return res.render('login')
+            }
         },
         logueado: (req,res)=>{
-            const usuario = req.body.usuario
+            // Variable para guardar errores
+            let errors = {};
+        
+            // Buscar el usuario por medio del mail
             usuarios.findOne({
-                where:[{ nombre_usuario: usuario }]
+                where: [{mail: req.body.email}]
             })
-            .then(resultado => {
-                req.session.usuario = resultado
-                if (req.body.recordame) {
-                    res.cookie("userId",resultado.id,{maxAge:10000*600*10})
+            .then( (resultado) => {
+                if(resultado==null){
+                    errors.login = "Email es incorrecto";
+                    res.locals.error = errors;
+                    return res.render('login') 
+                } else if (bcrypt.compareSync(req.body.contraseña, resultado.contraseña) == false){
+                    errors.login = "Contraseña Incorrecta";
+                    res.locals.errors = errors;
+                    return res.render('login') 
+                } else {
+                    req.session.usuario = resultado;
+
+                    if(req.body.recordame != undefined){
+                        res.cookie('userID', resultado.id, {maxAge: 1000 * 60 * 5});
+                    }
                 }
-                return res.redirect('/users/profile')
+                return res.redirect('/');
             })
-            .catch(err=> console.log(err))
-        },
+            .catch( err => console.log(err))     
+        },  
         logout:(req,res)=>{
             req.session.destroy()
             res.clearCookie('userId')
